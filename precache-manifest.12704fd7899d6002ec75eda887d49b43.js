e5d2497e0034579eb3a48f4aedb8b4be35eb47ff
Ryan Stubbs <rastubbs86@gmail.com>
2018-11-14 16:32:30 -0600

    Updates

---
+++ precache-manifest.12704fd7899d6002ec75eda887d49b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "f41f54ed642a56792174",
+    "url": "/RoboFriends/static/js/main.f41f54ed.chunk.js"
+  },
+  {
+    "revision": "8d026ad72220a023e306",
+    "url": "/RoboFriends/static/js/1.8d026ad7.chunk.js"
+  },
+  {
+    "revision": "f41f54ed642a56792174",
+    "url": "/RoboFriends/static/css/main.94094303.chunk.css"
+  },
+  {
+    "revision": "8d026ad72220a023e306",
+    "url": "/RoboFriends/static/css/1.e950808e.chunk.css"
+  },
+  {
+    "revision": "449c7bf3051c787b986a0cee1aaf050c",
+    "url": "/RoboFriends/index.html"
+  }
+];
